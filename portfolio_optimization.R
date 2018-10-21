b3aca47b6fce7a6cbdb5598a58449b162331525c
István Ladjánszki <istvan.ladjanszki@gmail.com>
2018-10-21 13:31:59 +0200

    * working and renamed

--- portfolio_optimization.R
+++ portfolio_optimization.R
@@ -7,8 +7,12 @@ library(quadprog)
 
 # GLOBAL PARAMETERS
 # Fraction of data used for learning
-learningFraction <- 0.7
+learningFraction <- 0.7 
 
+# Number of stocks used 
+nStock <- 5
+
+# GET DATA AND PREPROCESS ##############################################
 #Select and download 5 US stocks from separate indicies //financial/healthcare/services/tech/consumer goods
 GE <- pdfetch_YAHOO("GE", from = "2013-01-01", to = "2018-10-14")
 JPM <- pdfetch_YAHOO("JPM", from = "2013-01-01", to = "2018-10-14")
@@ -29,46 +33,41 @@ fullData <- drop_na(rawFullData)
 
 # Partition the data
 allLines <- nrow(fullData)
-#learningLines <- floor(nrow(fullData) * learningFraction)
 learningLines <- floor(allLines * learningFraction)
-learningLines
+#learningLines
 
+# In sample data to train on
 learningData <- fullData[1:learningLines, ]
+
+# Out of sample data to test on
 validationData <- fullData[learningLines+1:allLines, ]
 
-#Get the Covariance matrix of these returns
-#kovariancia <- cov(returns)
-kovariancia <- cov(learningData)
-
-kovariancia
-
-# The optimal portfolio
-nStock <- 5
-
-# Dmat is the correlation mx
-#Dmat <- matrix(c(356.25808, 12.31581, 261.88302, 12.31581, 27.24840, 18.50515, 261.88302, 18.50515,535.45960), nrow=3, ncol=3)
-Dmat <- matrix(kovariancia, nrow = nStock, ncol = nStock)
-Dmat
+# THE OPTIMAL PORTFOLIO SELECTION ##############################################
+#Get the Covariance matrix for the learning data only
+Dmat <- matrix(cov(learningData), nrow = nStock, ncol = nStock)
+#Dmat
 
 # Coeff vector for linear part
 dvec <- matrix(rep(0.0, nStock), nrow = nStock, ncol = 1)
-dvec
-
-#dvec <- matrix(c(9.33, 3.33, 9.07), nrow=3, ncol=1)
 #dvec
 
-#A.Equality <- matrix(c(1,1,1), ncol=1)
+# Constraints matrix
 A.Equality <- matrix(rep(1, nStock), ncol=1)
-#Amat <- cbind(A.Equality, dvec, diag(3), -diag(3))
 Amat <- cbind(A.Equality, diag(nStock), -diag(nStock))
-Amat
+#Amat
 
-#bvec <- c(1, 5.2, rep(0, 3), rep(-0.5, 3))
-bvec <- c(1, rep(0, nStock), rep(-0.5, nStock))
+# No short and no weight over 1
+bvec <- c(1, rep(0, nStock), rep(-1.0, nStock))
 bvec
 
+# Solv for optimal portfolio
 qp <- solve.QP(Dmat, dvec, Amat, bvec, meq=1)
-qp$solution
+optimalWeights <- qp$solution
+optimalWeights
+
+# Equal weight portfolio for benchmarking
+equalWeights <- matrix(rep(1/nStock, nStock), nrow = nStock, ncol = 1)
+equalWeights
 
 
 
